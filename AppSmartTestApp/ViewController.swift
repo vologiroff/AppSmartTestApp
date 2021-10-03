a87dcb4e0f8a192c6a1afb3ff4484b7b82533d38
vologiroff <volkant0778@ya.ru>
2021-10-03 16:19:43 +0300

    Initial Commit

---
+++ AppSmartTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppSmartTestApp
+//
+//  Created by Kantemir Vologirov on 10/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
